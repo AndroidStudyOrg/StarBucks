0163f6bbb033deec48988218c75764c6918e718a
wbham <wbham97@naver.com>
2024-02-10 20:21:13 +0900

    [Feat] - ProgressBar Drawable& ExtendedFAB & Generic
    - Json 파일에서 읽어오는 함수를 제네릭화
    - ProgressBar 커스텀. layer-list로 background와 progress로 구분
    - 스크롤 상태에 따라 FAB 모양 변경. ScrollView에 Listener를 달아서 scrollY에 따라 extend or shrink

--- app/src/main/java/org/shop/starbucks/HomeFragment.kt
+++ app/src/main/java/org/shop/starbucks/HomeFragment.kt
@@ -1,11 +1,14 @@
 package org.shop.starbucks
 
+import android.animation.ValueAnimator
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
+import org.shop.starbucks.data.Home
+import org.shop.starbucks.data.Menu
 import org.shop.starbucks.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -23,8 +26,58 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val homeData = context?.readData() ?: return
+        val homeData = context?.readData("home.json", Home::class.java) ?: return
+        val menuData = context?.readData("menu.json", Menu::class.java) ?: return
 
+        initAppBar(homeData)
+        initRecommendMenuList(homeData, menuData)
+        initBanner(homeData)
+        initFoodList(menuData)
+
+        binding.scrollView.setOnScrollChangeListener { v, _, scrollY, _, oldScrollY ->
+            if (scrollY == 0) {
+                binding.floatingActionButton.extend()
+            } else {
+                binding.floatingActionButton.shrink()
+            }
+        }
+    }
+
+    private fun initFoodList(menuData: Menu) {
+        binding.foodMenuList.titleTextView.text =
+            getString(R.string.food_menu_title)
+
+        menuData.food.forEach { foodItem ->
+            binding.foodMenuList.menuLayout.addView(MenuView(context = requireContext()).apply {
+                setTitle(foodItem.name)
+                setImageUrl(foodItem.image)
+            })
+        }
+    }
+
+    private fun initBanner(homeData: Home) {
+        binding.bannerLayout.bannerImageView.apply {
+            Glide.with(this).load(homeData.banner.image).into(this)
+            this.contentDescription = homeData.banner.contentDescription
+        }
+    }
+
+    private fun initRecommendMenuList(
+        homeData: Home,
+        menuData: Menu
+    ) {
+        binding.recommendMenuList.titleTextView.text =
+            getString(R.string.recommend_title, homeData.user.nickname)
+
+        menuData.coffee.forEach { menuItem ->
+            binding.recommendMenuList.menuLayout.addView(MenuView(context = requireContext()).apply {
+                setTitle(menuItem.name)
+                setImageUrl(menuItem.image)
+            })
+        }
+    }
+
+    private fun initAppBar(homeData: Home) {
         binding.appbarTitleTextView.text =
             resources.getString(R.string.appbar_title_text, homeData.user.nickname)
 
@@ -34,14 +87,16 @@ class HomeFragment : Fragment() {
             homeData.user.totalCount
         )
 
-        binding.appbarProgressBar.progress = homeData.user.starCount
         binding.appbarProgressBar.max = homeData.user.totalCount
 
         Glide.with(binding.appbarImageView).load(homeData.appbarImage).into(binding.appbarImageView)
 
-        binding.recommendMenuList.menuLayout.addView(MenuView(context = requireContext()).apply {
-            setTitle("디카페인 카페라떼")
-            setImageUrl("https://picsum.photos/100/100")
-        })
+        ValueAnimator.ofInt(0, homeData.user.starCount).apply {
+            duration = 1000
+            addUpdateListener {
+                binding.appbarProgressBar.progress = it.animatedValue as Int
+            }
+            start()
+        }
     }
 }
